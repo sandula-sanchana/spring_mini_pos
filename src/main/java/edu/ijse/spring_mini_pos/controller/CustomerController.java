4696335cf9b58ebd1b01ff63fde5c4e1d85ee54c
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-11 18:14:34 +0800

    explain @restcontroller , request mapping and cross origin annotations

--- src/main/java/edu/ijse/spring_mini_pos/controller/CustomerController.java
+++ src/main/java/edu/ijse/spring_mini_pos/controller/CustomerController.java
@@ -1,4 +1,17 @@
 package edu.ijse.spring_mini_pos.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // Marks this class as a REST controller that handles HTTP requests
+// convert class in to a rest controller that returns data as response (json)
+@RequestMapping("app/v1/customer") // Class-level annotation used to define the base URL
+// for all endpoints within this controller.
+
+@CrossOrigin // Enables Cross-Origin Resource Sharing (CORS)
+// allowing frontend applications hosted on different origins
+// to communicate with this backend.
+
 public class CustomerController {
 }
