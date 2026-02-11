280c3420bf0604cf9cd159234c1736ee7c86b900
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-11 15:26:49 +0800

    first commit

---
+++ src/main/java/edu/ijse/spring_mini_pos/SpringMiniPosApplication.java
@@ -0,0 +1,13 @@
+package edu.ijse.spring_mini_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMiniPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMiniPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ijse/spring_mini_pos/SpringMiniPosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ijse.spring_mini_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
