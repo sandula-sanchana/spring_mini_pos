07d658b26c6bd9f934fc5e249e9cd1cf3630b7ce
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-11 21:00:28 +0800

    add item html http methods and item entity

--- src/main/java/edu/ijse/spring_mini_pos/entity/Item.java
+++ src/main/java/edu/ijse/spring_mini_pos/entity/Item.java
@@ -1,15 +1,21 @@
-package edu.ijse.spring_mini_pos.dto;
+package edu.ijse.spring_mini_pos.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class ItemDTO {
+public class Item {
+    @Id
     private String id;
     private String name;
     private String qty;
