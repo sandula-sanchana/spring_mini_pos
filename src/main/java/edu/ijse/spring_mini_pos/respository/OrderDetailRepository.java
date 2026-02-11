e890f2d775fa5042bb1bc4044fb33bd891035913
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-11 23:13:51 +0800

    add order detail repo and order repo

---
+++ src/main/java/edu/ijse/spring_mini_pos/respository/OrderDetailRepository.java
@@ -0,0 +1,10 @@
+package edu.ijse.spring_mini_pos.respository;
+
+import edu.ijse.spring_mini_pos.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findAllByOrderId(String orderId);
+}
