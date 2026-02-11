e411260638d688024dac09b5a84f97e3593a98d0
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-11 18:43:50 +0800

    add @service annotation for customer service impl

--- src/main/java/edu/ijse/spring_mini_pos/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/ijse/spring_mini_pos/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,9 @@
 package edu.ijse.spring_mini_pos.service.impl;
 
 import edu.ijse.spring_mini_pos.service.CustomerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Override
