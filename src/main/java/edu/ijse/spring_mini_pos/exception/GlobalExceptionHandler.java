2d5c3561f78238f61ec1a38d77361b668a5b32da
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-12 13:47:39 +0800

    restcontrolleradvice

---
+++ src/main/java/edu/ijse/spring_mini_pos/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package edu.ijse.spring_mini_pos.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
