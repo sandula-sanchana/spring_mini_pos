4432a708d059bdc8b6d53c8f6fd44e28f4618688
sandula-sanchana <sandulasanchana@gmail.com>
2026-02-12 14:34:11 +0800

    @valid for cus save

--- src/main/java/edu/ijse/spring_mini_pos/controller/CustomerController.java
+++ src/main/java/edu/ijse/spring_mini_pos/controller/CustomerController.java
@@ -4,6 +4,7 @@ import edu.ijse.spring_mini_pos.dto.CustomerDTO;
 import edu.ijse.spring_mini_pos.service.CustomerService;
 import edu.ijse.spring_mini_pos.service.impl.CustomerServiceImpl;
 import edu.ijse.spring_mini_pos.util.APIResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class CustomerController {
 
 
     @PostMapping
-    public ResponseEntity<APIResponse<String>> saveCustomer(@RequestBody CustomerDTO customerDTO) {
+    public ResponseEntity<APIResponse<String>> saveCustomer(@RequestBody @Valid CustomerDTO customerDTO) {
         customerService.saveCustomer(customerDTO);
         return new ResponseEntity(new APIResponse<>(
                 201,"customer saved",null
